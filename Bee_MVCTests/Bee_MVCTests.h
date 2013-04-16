bfb260fcd2debf57a52d118d8b5ce5474dc203c1
i5ting <i5ting@126.com>
2013-04-16 12:59:03 +0800

    Initial Commit

---
+++ Bee_MVCTests/Bee_MVCTests.h
@@ -0,0 +1,13 @@
+//
+//  Bee_MVCTests.h
+//  Bee_MVCTests
+//
+//  Created by sang on 4/16/13.
+//  Copyright (c) 2013 alfred sang. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Bee_MVCTests : SenTestCase
+
+@end
---
+++ Bee_MVCTests/Bee_MVCTests.m
@@ -0,0 +1,32 @@
+//
+//  Bee_MVCTests.m
+//  Bee_MVCTests
+//
+//  Created by sang on 4/16/13.
+//  Copyright (c) 2013 alfred sang. All rights reserved.
+//
+
+#import "Bee_MVCTests.h"
+
+@implementation Bee_MVCTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Bee_MVCTests");
+}
+
+@end
